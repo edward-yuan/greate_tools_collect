a37a18e82159be47ff91cd9c6ab158de6cff9949
edward-yuan <zhangyuan19870129@gmail.com>
2016-10-29 08:42:10 +0800

    new script

---
+++ script/connoracletofile.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+'''
+- 文件名：    connoracletofile.py
+- 作者：      杰出的城市农夫一代
+- 时间：      2016年10月28日14:29:22
+- 作用：      从数据库里面查询数据导出到文本里面
+- 开发工具：  python 3.4.4,cx_Oracle 5.2.1(模块)
+- TODO：      暂时没有
+- 耗时：      1900ms
+'''
+import sys
+import cx_Oracle
+import datetime
+
+starttime = datetime.datetime.now()
+
+SQL="select t.custno||','||t.certificatetype||','||t.certificateno from ACCT_CUST t where t.invtp='1'"
+
+#connection = cx_Oracle.Connection("kd_sale/1@orcl")
+connection=cx_Oracle.connect('kd_sale','1','192.168.240.117:1521/RACTEST')
+
+cursor = connection.cursor()
+
+#连接数据库，执行查询
+try:
+    cursor.execute(SQL)
+    rs=cursor.fetchall()   
+except cx_Oracle.DatabaseError as exc:
+    print(exc)
+finally:
+    connection.close()
+
+#开始写文件    
+f = open('../output/test-etrading.dat', 'w')
+f.write('custno,certificatetype,certificateno\n')
+for i in rs:
+    f.write(i[0]+'\n')
+
+f.close()
+endtime = datetime.datetime.now()
+#打印耗时
+print (endtime - starttime)
--- script/makedatatofile.py
+++ script/makedatatofile.py
@@ -1,20 +1,43 @@
 # encoding: utf-8
 '''
-- 文件名：makedatatofile.py
-- 作者：  杰出的城市农夫一代
-- 时间：  2016年10月25日15:50:00
-- 作用：  批量生成100000条数据,文本文件
-- TODO：  暂时没有
-- 耗时：  -
+- 文件名：    makedatatofile.py
+- 作者：      杰出的城市农夫一代
+- 时间：      2016年10月25日15:50:00
+- 作用：      批量生成100000条数据,文本文件
+- 开发工具：  python 3.4.4
+- TODO：      暂时没有
+- 耗时：      300ms
 '''
 import datetime
 starttime = datetime.datetime.now()
 
 f = open('../output/test.dat', 'w')
-string='430382!g_userid:8888,g_userpwd:=,g_userway:2,g_stationaddr:MAC[],g_sysid:8,g_menuid:12,g_funcid:430382,g_checksno:0,g_chkuserid:,g_confirmaction:0,g_confirmlevel:0,contractno:TT2013042310000051,applicationno:TT2013042110000000,distributorcode:225,businesscode:022,fundcode:420006,isforcedeal:1,applicationamount:30000,buyflag:1,'
 
-for i in range(0,10000):
-	f.write(string+'\n')
+
+lbm_no='487209'
+global_string='g_funcid:'+lbm_no+',g_orgid:0,kcbp:KCBP00,g_confirmaction:0,g_sysid:4,g_userid:8888,g_menuid:0,g_confirmlevel:0,g_userpwd:,g_chkuserid:0,g_userway:2,g_stationaddr:192.168.77.167:2016D86E0798,g_checksno:0,'
+
+#487216
+string1='custno:30797194,custno_crm:000003252256,fundcode:161608,detailflag:1'
+#430312
+string2='custno:30797194,fundcode:,businesscode:,paycenterid:,begindate:20160807,enddate:20160910'
+#430313
+string3='begindate:20160201,enddate:20160301,channelid:,custno:30797194,businesscode:,status:,paycenterid:,fundcode:'
+#430305
+string4='custno:30797194,flag:2'
+#487207
+string5='custno_crm:000003252256,begindate:20150101,enddate:20150401,fundcode:,businesscode:'
+#487208
+string6='custno_crm:000003252256,begindate:20150101,enddate:20150401,fundcode:'
+#487209
+string7='custno:30797194,custno_crm:000003252256,detailflag:'
+
+string=string7
+
+exec_string=lbm_no+'!'+global_string+string
+
+for i in range(0,100000):
+	f.write(exec_string+'\n')
 	i += i
 
 f.close()
