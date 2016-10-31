a7c2004822a4a7ebf5afe7a221f76d4b7663ee91
edward-yuan <zhangyuan19870129@gmail.com>
2016-10-31 11:02:22 +0800

    pep8 规范

--- script/makedatatofile.py
+++ script/makedatatofile.py
@@ -14,34 +14,35 @@ starttime = datetime.datetime.now()
 f = open('../output/test.dat', 'w')
 
 
-lbm_no='487209'
-global_string='g_funcid:'+lbm_no+',g_orgid:0,kcbp:KCBP00,g_confirmaction:0,g_sysid:4,g_userid:8888,g_menuid:0,g_confirmlevel:0,g_userpwd:,g_chkuserid:0,g_userway:2,g_stationaddr:192.168.77.167:2016D86E0798,g_checksno:0,'
+lbm_no = '487209'
+global_string = 'g_funcid:' + lbm_no + \
+    ',g_orgid:0,kcbp:KCBP00,g_confirmaction:0,g_sysid:4,g_userid:8888,g_menuid:0,g_confirmlevel:0,g_userpwd:,g_chkuserid:0,g_userway:2,g_stationaddr:192.168.77.167:2016D86E0798,g_checksno:0,'
 
-#487216
-string1='custno:30797194,custno_crm:000003252256,fundcode:161608,detailflag:1'
-#430312
-string2='custno:30797194,fundcode:,businesscode:,paycenterid:,begindate:20160807,enddate:20160910'
-#430313
-string3='begindate:20160201,enddate:20160301,channelid:,custno:30797194,businesscode:,status:,paycenterid:,fundcode:'
-#430305
-string4='custno:30797194,flag:2'
-#487207
-string5='custno_crm:000003252256,begindate:20150101,enddate:20150401,fundcode:,businesscode:'
-#487208
-string6='custno_crm:000003252256,begindate:20150101,enddate:20150401,fundcode:'
-#487209
-string7='custno:30797194,custno_crm:000003252256,detailflag:'
+# 487216
+string1 = 'custno:30797194,custno_crm:000003252256,fundcode:161608,detailflag:1'
+# 430312
+string2 = 'custno:30797194,fundcode:,businesscode:,paycenterid:,begindate:20160807,enddate:20160910'
+# 430313
+string3 = 'begindate:20160201,enddate:20160301,channelid:,custno:30797194,businesscode:,status:,paycenterid:,fundcode:'
+# 430305
+string4 = 'custno:30797194,flag:2'
+# 487207
+string5 = 'custno_crm:000003252256,begindate:20150101,enddate:20150401,fundcode:,businesscode:'
+# 487208
+string6 = 'custno_crm:000003252256,begindate:20150101,enddate:20150401,fundcode:'
+# 487209
+string7 = 'custno:30797194,custno_crm:000003252256,detailflag:'
 
-string=string7
+string = string7
 
-exec_string=lbm_no+'!'+global_string+string
+exec_string = lbm_no + '!' + global_string + string
 
-for i in range(0,100000):
-	f.write(exec_string+'\n')
-	i += i
+for i in range(0, 100000):
+    f.write(exec_string + '\n')
+    i += i
 
 f.close()
 
 endtime = datetime.datetime.now()
-#打印耗时
-print (endtime - starttime)
+# 打印耗时
+print(endtime - starttime)
